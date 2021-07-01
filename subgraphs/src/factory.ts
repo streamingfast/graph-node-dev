5c6c2b7cc3bca45ea8dc020dbf7b2f5f5a82fb39
Matthieu Vachon <matthieu.vachon@dfuse.io>
2021-07-01 19:28:29 -0400

    feat: added some contracts

--- subgraphs/src/factory.ts
+++ subgraphs/src/factory.ts
@@ -1,12 +1,11 @@
-import { BigDecimal, BigInt, Bytes, log } from "@graphprotocol/graph-ts";
-import { Transfer } from "../../generated/Factory/Factory";
-import { Payment } from "../../generated/schema";
+import { Transfer } from "../../generated/Factory/Factory"
+import { Payment } from "../../generated/schema"
 
 export function handleTransfer(event: Transfer): void {
-    let payment = new Payment(event.block.number.toString());
-    payment.from = event.transaction.from.toHex();
-    payment.to = event.transaction.to.toHex();
-    payment.value = event.transaction.value;
-    
-    payment.save();
+  let payment = new Payment(event.block.number.toString())
+  payment.from = event.transaction.from.toHex()
+  payment.to = event.transaction.to.toHex()
+  payment.value = event.transaction.value
+
+  payment.save()
 }
