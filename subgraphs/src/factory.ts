1a7c6592418d252e0c18bff832e7044f73dd78a0
colindickson <colin.dickson@gmail.com>
2021-07-15 10:20:56 -0400

    id is now block number

--- subgraphs/src/factory.ts
+++ subgraphs/src/factory.ts
@@ -3,8 +3,7 @@ import { Transfer } from "../../generated/Factory/Factory";
 import { Payment } from "../../generated/schema";
 
 export function handleTransfer(event: Transfer): void {
-
-    let payment = new Payment("1");
+    let payment = new Payment(event.block.number.toString());
     payment.from = event.transaction.from.toHex();
     payment.to = event.transaction.to.toHex();
     payment.value = event.transaction.value;
