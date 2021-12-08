9097fca2cda4d5abb34fd294e0608fe9818469d5
Stéphane Duchesneau <stephane.duchesneau@streamingfast.io>
2021-12-08 14:56:43 -0500

    add call-based transfer subgraph configs and hooks for mainnet/ropsten

--- subgraphs/ethereum/transfer/src/transfer.ts
+++ subgraphs/ethereum/transfer/src/transfer.ts
@@ -1,11 +1,19 @@
-import { Transfer } from "../generated/transfer/transfer"
+import { Transfer, TransferCall } from "../generated/transfer/transfer"
 import { Payment } from "../generated/schema"
 
-export function handleTransfer(event: Transfer): void {
+export function handleTransferEvent(event: Transfer): void {
   let payment = new Payment(event.block.number.toString())
   payment.from = event.transaction.from.toHex()
   payment.to = event.transaction.to!.toHex()
   payment.value = event.transaction.value
 
+  payment.save()
+}
+
+export function handleTransferCall(call: TransferCall): void {
+  let payment = new Payment(call.block.number.toString())
+  payment.from = call.from.toHex()
+  payment.to = call.inputs._to.toHex()
+  payment.value = call.inputs._value
   payment.save()
 }
